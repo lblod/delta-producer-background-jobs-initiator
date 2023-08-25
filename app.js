b42c3b8653c5d84a199f5a2440bea6ce48534382
felix <ruiz.felixruiz@gmail.com>
2023-08-25 10:55:35 +0200

    refactor app.js

--- app.js
+++ app.js
@@ -25,146 +25,22 @@ async function init() {
   const configFile = fs.readFileSync(CONFIG_FILE_JSON);
   const config = JSON.parse(configFile);
 
-  for (const conf of config) {
-    ensureDefaultsConfig(config);
-    validateConfig(config);
-    let {
-      name,
-      jobsGraph,
-      dumpFileCreationJobOperation,
-      initialPublicationGraphSyncJobOperation,
-      healingJobOperation,
-      cronPatternDumpJob,
-      cronPatternHealingJob,
-      startInitialSync,
-      disableDumpFileCreation,
-      disableHealingJobOperation,
-      healShouldNotWaitForInitialSync,
-      errorCreatorUri
-    } = conf;
+  for (let conf of config) {
+    ensureDefaultsConfig(conf);
+    validateConfig(conf);
 
-    console.log(`setup background job ${name}`);
+    console.log(`setup background job ${conf.name}`);
 
     console.info(
-      `INFO: start initial sync for ${name} is set to: ${startInitialSync}`
+      `INFO: start initial sync for ${conf.name} is set to: ${conf.startInitialSync}`
     );
 
-    if (startInitialSync) {
-      runInitialSyncPublicationGraphJob(
-        jobsGraph,
-        initialPublicationGraphSyncJobOperation,
-        dumpFileCreationJobOperation,
-        healingJobOperation,
-        disableDumpFileCreation, disableHealingJobOperation, errorCreatorUri
-      );
+    if (conf.startInitialSync) {
+      runInitialSyncPublicationGraphJob(conf);
     }
 
-    console.info(
-      `INFO: disable dump file creation for ${name} set to: ${!!disableDumpFileCreation}`
-    );
-
-    if (!disableDumpFileCreation) {
-      console.info(
-        `INFO: Scheduling dump file creation for ${name}, with cronPatternDumpJob set to: ${cronPatternDumpJob}`
-      );
-      new CronJob(
-        cronPatternDumpJob || DEFAULT_CRON_PATTERN_JOB,
-        async function() {
-          const now = new Date().toISOString();
-          console.info(`First check triggered by cron job for ${name} at ${now}`);
-          await runDumpPublicationGraphJob(
-            jobsGraph,
-            dumpFileCreationJobOperation,
-            initialPublicationGraphSyncJobOperation,
-            healingJobOperation,
-            errorCreatorUri
-          );
-        },
-        null,
-        true
-      );
-    }
-    console.info(
-      `INFO: disable Healing Job Operation for ${name} set to: ${!!disableHealingJobOperation}`
-    );
-
-    if (!disableHealingJobOperation) {
-      console.info(
-        `INFO: Scheduling healing for ${name}, with cronPatternHealingJob set to: ${cronPatternHealingJob}`
-      );
-      new CronJob(
-        cronPatternHealingJob || DEFAULT_CRON_PATTERN_JOB,
-        async function() {
-          const now = new Date().toISOString();
-          console.info(`First check triggered by cron job for ${name} at ${now}`);
-          await runHealPublicationGraphJob(
-            jobsGraph,
-            healingJobOperation,
-            initialPublicationGraphSyncJobOperation,
-            dumpFileCreationJobOperation,
-            healShouldNotWaitForInitialSync,
-            errorCreatorUri
-          );
-        },
-        null,
-        true
-      );
-    }
-
-    /*
-     * ENDPOINTS CURRENTLY MEANT FOR DEBUGGING
-     */
-    if (dumpFileCreationJobOperation) {
-      app.post(`/${name}/dump-publication-graph-jobs`, async function(_, res) {
-        const jobUri = await createJob(jobsGraph, dumpFileCreationJobOperation);
-        await scheduleTask(jobsGraph, jobUri, DUMP_FILE_CREATION_TASK_OPERATION);
-        res.send({ msg: `Dump publication graph job ${jobUri} triggered` });
-      });
-      app.delete(`/${name}/dump-publication-graph-jobs`, async function(_, res) {
-        const jobs = await getJobs(dumpFileCreationJobOperation);
-        await cleanupJobs(jobs);
-        res.send({ msg: "Dump publication graph job cleaned" });
-      });
-    }
-
-    if (healingJobOperation) {
-      app.post(`/${name}/healing-jobs`, async function(_, res) {
-        const jobUri = await createJob(jobsGraph, healingJobOperation);
-        await scheduleTask(jobsGraph, jobUri, HEALING_TASK_OPERATION);
-        res.send({ msg: `Healing job ${jobUri} triggered` });
-      });
-
-      app.delete(`/${name}/healing-jobs`, async function(_, res) {
-        const jobs = await getJobs(jobsGraph, healingJobOperation);
-        await cleanupJobs(jobs);
-        res.send({ msg: "Healing job cleaned" });
-      });
-    }
-    if (initialPublicationGraphSyncJobOperation) {
-      app.post(`/${name}/initial-sync-jobs`, async function(_, res) {
-        const jobUri = await createJob(
-          jobsGraph,
-          initialPublicationGraphSyncJobOperation
-        );
-        await scheduleTask(
-          jobsGraph,
-          jobUri,
-          INITIAL_PUBLICATION_GRAPH_SYNC_TASK_OPERATION
-        );
-        res.send({ msg: `Sync jobs started ${jobUri}` });
-      });
-
-      app.delete(`/${name}/initial-sync-jobs`, async function(_, res) {
-        const jobs = await getJobs(
-          jobsGraph,
-          initialPublicationGraphSyncJobOperation
-        );
-        await cleanupJobs(jobs);
-        res.send({
-          msg: `Sync jobs cleaned ${jobs.map((j) => j.jobUri).join(", ")}`,
-        });
-      });
-    }
+    scheduleJobs(conf);
+    setupDebugEndpoints(conf);
   }
 }
 
@@ -194,6 +70,8 @@ function validateConfig(config) {
 }
 
 function ensureDefaultsConfig(config) {
+  config.cronPatternHealingJob = config.cronPatternHealingJob || DEFAULT_CRON_PATTERN_JOB,
+  config.cronPatternDumpJob = config.cronPatternDumpJob || DEFAULT_CRON_PATTERN_JOB;
   config.disableDumpFileCreation = config.disableDumpFileCreation || false;
   config.disableHealingJobOperation = config.disableHealingJobOperation || false;
   config.healShouldNotWaitForInitialSync = config.healShouldNotWaitForInitialSync || false;
@@ -204,4 +82,81 @@ function ensureDefaultsConfig(config) {
     }
   }
   return config;
+}
+
+function scheduleJobs(conf) {
+  console.info(
+    `INFO: Is dump file creation disabled for ${conf.name}? : ${!!conf.disableDumpFileCreation}`
+  );
+
+  if (!conf.disableDumpFileCreation) {
+    console.info(
+      `INFO: Scheduling dump file creation for ${conf.name}, with cronPatternDumpJob set to: ${conf.cronPatternDumpJob}`
+    );
+    new CronJob(conf.cronPatternDumpJob,
+      async function() {
+        const now = new Date().toISOString();
+        console.info(`Dump file job triggered for ${conf.name} at ${now}`);
+        await runDumpPublicationGraphJob(conf);
+      },
+      null,
+      true
+    );
+  }
+  console.info(
+    `INFO: Is Healing Job Operation for ${conf.name} disabled? ${!!conf.disableHealingJobOperation}`
+  );
+
+  if (!conf.disableHealingJobOperation) {
+    console.info(
+      `INFO: Scheduling healing for ${conf.name}, with cronPatternHealingJob set to: ${conf.cronPatternHealingJob}`
+    );
+    new CronJob(conf.cronPatternHealingJob,
+      async function() {
+        const now = new Date().toISOString();
+        console.info(`Healing job triggered for ${conf.name} at ${now}`);
+        await runHealPublicationGraphJob(conf);
+      },
+      null,
+      true
+    );
+  }
+}
+
+function setupDebugEndpoints(conf) {
+
+  app.post(`/${conf.name}/dump-publication-graph-jobs`, async function(_, res) {
+    runDumpPublicationGraphJob(conf, true);
+    res.send({ msg: `Dump publication graph job triggered` });
+  });
+
+  app.delete(`/${conf.name}/dump-publication-graph-jobs`, async function(_, res) {
+      const jobs = await getJobs(conf.dumpFileCreationJobOperation);
+      await cleanupJobs(jobs);
+      res.send({ msg: "Dump publication graph job cleaned" });
+    });
+
+  app.post(`/${conf.name}/healing-jobs`, async function(_, res) {
+    runHealPublicationGraphJob(conf, true);
+    res.send({ msg: `Healing job triggered`});
+  });
+
+  app.delete(`/${conf.name}/healing-jobs`, async function(_, res) {
+    const jobs = await getJobs(conf.healingJobOperation);
+    await cleanupJobs(jobs);
+    res.send({ msg: "Healing job cleaned" });
+  });
+
+  app.post(`/${conf.name}/initial-sync-jobs`, async function(_, res) {
+    runInitialSyncPublicationGraphJob(conf, true);
+    res.send({ msg: `Initial sync job started` });
+  });
+
+  app.delete(`/${conf.name}/initial-sync-jobs`, async function(_, res) {
+    const jobs = await getJobs(conf.initialPublicationGraphSyncJobOperation);
+    await cleanupJobs(jobs);
+    res.send({
+      msg: `Sync jobs cleaned ${jobs.map((j) => j.jobUri).join(", ")}`
+    });
+  });
 }
